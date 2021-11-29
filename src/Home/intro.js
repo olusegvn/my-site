ef31755dab574c603183113ac8924802c5cea1a7
Yinka Olusegun <olusegvn@gmail.com>
2021-11-29 23:51:47 +0100

    update

---
+++ src/Home/intro.js
@@ -0,0 +1,11 @@
+import introVideo from '../Images/intro.mp4'
+
+const Intro = () => {
+    return ( 
+        <div className="mainIntro">
+            <video src={introVideo} alt="dfdgetsb vxbetsgretsbdf xgvdfa"  autoPlay loop/>  
+        </div>
+     );
+}
+ 
+export default Intro;
